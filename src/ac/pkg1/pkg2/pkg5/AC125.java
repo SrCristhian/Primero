341a4a7a374837ee7ee78eac9153723135084431
2dama <2dama@PC221003.informatica.edu>
2016-09-15 14:59:47 +0100

    Inicio del programa, hay que realizar cambios.

---
+++ src/ac/pkg1/pkg2/pkg5/AC125.java
@@ -0,0 +1,16 @@
+/*
+ * AC 1.2.5
+ * Autor: Cristian Ramos Baute
+ * Fecha: 14/09/2016
+ * Descripcion: Realiza un programa que cree un directorio y un fichero en el
+ * escritorio y que a continuacion te pregunte si desea borrarlo.
+ */ 
+package ac.pkg1.pkg2.pkg5;
+import java.io.File;
+public class AC125 {
+    public static void main(String[] args) {
+        File archivo = new File("C:\\Users\\2dama\\Desktop\\AC 2.2.5.txt");
+        File fichero = new File("C:\\Users\\2dama\\Desktop\\AC 2.2.5");
+        archivo.
+    }
+}
